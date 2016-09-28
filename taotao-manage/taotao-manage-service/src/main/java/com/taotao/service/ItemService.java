0eaf580f63fb3c93b463b0916b730b4f081b3cef
leileiyuan <yuanleilei628@163.com>
2016-09-28 18:34:11 +0800

    商品列表；日志

--- taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/ItemService.java
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/ItemService.java
@@ -1,29 +1,49 @@
 package com.taotao.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.abel533.entity.Example;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.taotao.common.EasyUIResult;
+import com.taotao.dao.ItemMapper;
 import com.taotao.pojo.Item;
 import com.taotao.pojo.ItemDesc;
 
 @Service
 public class ItemService extends BaseService<Item> {
-	
+
+	@Autowired
+	private ItemMapper itemMapper;
+
 	@Autowired
 	private ItemDescService itemDescService;
-	
-	public boolean saveItem(Item item, String desc){
+
+	public boolean saveItem(Item item, String desc) {
 		// 初始值
 		item.setStatus(1);
-		item.setId(null);	// 出于安全考虑，强制设置id为null，通过数据库自增得到
-		//this.itemService.save(item);
+		item.setId(null); // 出于安全考虑，强制设置id为null，通过数据库自增得到
+		// this.itemService.save(item);
 		int count1 = super.save(item);
-		
+
 		ItemDesc itemDesc = new ItemDesc();
 		itemDesc.setItemId(item.getId());
 		itemDesc.setItemDesc(desc);
 		int count2 = this.itemDescService.save(itemDesc);
-		
+
 		return (count1 == 1) && (count2 == 1);
+	}
+
+	public EasyUIResult queryItemList(int page, int rows) {
+		// 设置分页参数
+		PageHelper.startPage(page, rows);
+		Example example = new Example(Item.class);
+		example.setOrderByClause("updated DESC");
+		List<Item> list = itemMapper.selectByExample(example);
+		PageInfo<Item> info = new PageInfo<>(list);
+		return new EasyUIResult(info.getTotal(), info.getList());
 	}
 }
--- taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/ItemController.java
@@ -1,6 +1,8 @@
 package com.taotao.controller;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.taotao.common.EasyUIResult;
 import com.taotao.pojo.Item;
 import com.taotao.service.ItemService;
 
@@ -16,24 +19,41 @@ import com.taotao.service.ItemService;
 @RequestMapping("/item")
 public class ItemController {
 
+	private static Logger logger = LoggerFactory.getLogger(ItemController.class);
+
 	@Autowired
 	private ItemService itemService;
 
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> saveItem(Item item, @RequestParam("desc") String desc) {
 		try {
+			logger.info("新增商品，item = {}, desc = {}", item, desc);
 			if (StringUtils.isEmpty(item.getTitle())) {
 				// 参数有误 400
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 			}
 			boolean bool = this.itemService.saveItem(item, desc);
 			if (!bool) {
+				logger.info("新增商品失败,item = {}", item);
 				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 			}
+			logger.info("新增商品成功,itemId = {}", item.getId());
 			return ResponseEntity.status(HttpStatus.CREATED).build();
+		} catch (Exception e) {
+			logger.error("新增商品失败", e);
+		}
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<EasyUIResult> queryItemList(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "rows", defaultValue = "30") int rows) {
+		try {
+			EasyUIResult easyUIResult = itemService.queryItemList(page, rows);
+			return ResponseEntity.ok(easyUIResult);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 	}
 }
