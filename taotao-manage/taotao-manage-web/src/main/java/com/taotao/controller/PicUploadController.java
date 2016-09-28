d590a198756d1bea29035ff1fb8c8a77eea5f03e
leileiyuan <yuanleilei628@163.com>
2016-09-28 13:58:25 +0800

    del import

--- taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/PicUploadController.java
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/PicUploadController.java
@@ -14,7 +14,6 @@ import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
