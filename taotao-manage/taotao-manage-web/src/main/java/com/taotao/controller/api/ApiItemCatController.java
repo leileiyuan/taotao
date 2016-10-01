0a7ca90ee54f1940e8515581487512ee3cac4f1c
leileiyuan <yuanleilei268@163.com>
2016-10-01 10:49:01 +0800

    解决首页 类目跨域问题。存在乱码问题，数据解析转换存在乱码问题

--- taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/api/ApiItemCatController.java
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/controller/api/ApiItemCatController.java
@@ -1,17 +1,21 @@
 package com.taotao.controller.api;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taotao.common.ItemCatResult;
 import com.taotao.service.ItemCatService;
 
 /**
  * 对外提供的接口
+ * 
  * @author Administrator
  *
  */
@@ -21,15 +25,23 @@ public class ApiItemCatController {
 	@Autowired
 	private ItemCatService itemCatService;
 
+	private static final ObjectMapper MAPPER = new ObjectMapper();
+
 	/**
-	 *  查询所有类目数据
+	 * 查询所有类目数据
+	 * 
 	 * @return
 	 */
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<ItemCatResult> queryItemCatList() {
+	public ResponseEntity<String> queryItemCatList(
+			@RequestParam(value = "callback", required = false) String callback) {
 		try {
 			ItemCatResult itemCatResult = itemCatService.queryAllToTree();
-			return ResponseEntity.ok(itemCatResult);
+			String json = MAPPER.writeValueAsString(itemCatResult);
+			if (StringUtils.isEmpty(callback)) {
+				return ResponseEntity.ok(json);
+			}
+			return ResponseEntity.ok(callback + "(" + json + ")");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
