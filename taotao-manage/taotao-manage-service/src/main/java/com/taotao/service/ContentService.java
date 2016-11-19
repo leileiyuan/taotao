0aa583fb8ef6a52d5c1d088301362ce670f4fe7f
leileiyuan <yuanleilei268@163.com>
2016-11-19 16:58:43 +0800

    内容管理，根据categoryId查询列表

--- taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/ContentService.java
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/service/ContentService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.EasyUIResult;
 import com.taotao.dao.ContentMapper;
@@ -16,6 +17,7 @@ public class ContentService extends BaseService<Content> {
 	private ContentMapper contentMapper;
 
 	public EasyUIResult queryListByCategoryId(long categoryId, int page, int rows) {
+		PageHelper.startPage(page, rows);
 		List<Content> list = this.contentMapper.queryContentList(categoryId);
 		PageInfo<Content> pageInfo = new PageInfo<>(list);
 		return new EasyUIResult(pageInfo.getTotal(), pageInfo.getList());
