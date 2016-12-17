e85b8d34302c4cba09e4b183c2036ae71d6ed1cb
leileiyuan <yuanleilei268@163.com>
2016-12-17 19:49:30 +0800

    注册功能实现，密码采用apache common codee的MD5加密

--- taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso/src/main/java/com/taotao/sso/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.taotao.sso.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.taotao.sso.pojo.User;
 import com.taotao.sso.service.UserService;
 
 @Controller
@@ -29,12 +34,30 @@ public class UserController {
 			if (bool == null) {
 				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
 			}
-			
+
 			// bool值，不存在为true,
 			return ResponseEntity.ok(!bool);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+	}
+
+	@RequestMapping(value = "/doRegister", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> doRegister(User user) {
+		Map<String, Object> result = new HashMap<>();
+		try {
+			Boolean bool = this.userService.doRegister(user);
+			if (bool) {
+				result.put("status", "200");
+			} else {
+				result.put("data", "注册失败");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.put("data", "注册失败");
+		}
+		return result;
 	}
 }
--- taotao-sso/src/main/java/com/taotao/sso/service/UserService.java
+++ taotao-sso/src/main/java/com/taotao/sso/service/UserService.java
@@ -1,5 +1,8 @@
 package com.taotao.sso.service;
 
+import java.util.Date;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +14,10 @@ public class UserService {
 	@Autowired
 	private UserMapper userMapper;
 
-	/** 检查对应类型的用户是否存在:用户名是否存在，手机是否存在，邮箱是否存在。<br>
-	 *  存在返回false，不存在返回true */
+	/**
+	 * 检查对应类型的用户是否存在:用户名是否存在，手机是否存在，邮箱是否存在。<br>
+	 * 存在返回false，不存在返回true
+	 */
 	public Boolean check(String param, Integer type) {
 
 		User record = new User();
@@ -32,5 +37,14 @@ public class UserService {
 		}
 
 		return this.userMapper.selectOne(record) == null;
+	}
+
+	public Boolean doRegister(User user) {
+		user.setId(null);
+		user.setCreated(new Date());
+		user.setUpdated(user.getCreated());
+		// 密码MD5加密，
+		user.setPassword(DigestUtils.md5Hex(user.getPassword()));
+		return this.userMapper.insert(user) == 1;
 	}
 }
